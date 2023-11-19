202d6535f253dc1bfc273c94dad8a9a862c49d11
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-11-19 14:03:59 +0530

    Initial commit done to save file structure.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+    return (
+        <React.Fragment>
+            <h2>Welcome to Slack Clone</h2>
+        </React.Fragment>
+    )
+}
+
+export default App
