4de6590c24e76792ea735b8d7cd06b2a7966a003
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-11-19 15:04:14 +0530

    Add. Included login and signup Components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import LoginComponent from './components/LoginComponent/LoginComponent';
+import SignupComponent from './components/SignUpComponent/SignUpComponent';
+import UserDataComponent from './components/DashboardComponent/DashboardComponent';
 
-function App() {
 
+const App = () => {
     return (
-        <React.Fragment>
-            <h2>Welcome to Slack Clone</h2>
-        </React.Fragment>
-    )
+        <Router>
+            <div className="App">
+                <nav className='navbar navbar-expand-lg navbar-light fixed-top'>
+                    <div className='container'> 
+                        <Link className='navbar-brand' to={'/login'}>
+                            Slack Clone
+                        </Link>
+
+                    </div>
+                </nav>
+
+                    <div className='auth-wrapper'>
+                        <div className='auth-inner'>
+                            <Routes>
+                                <Route exact path='/' element={<LoginComponent/>}/>
+                                <Route path='/login' element={<LoginComponent/>}/>
+                                <Route path='/signup' element={<SignupComponent/>}/>
+                                <Route path='/dashboard' element={<UserDataComponent/>}/>
+                            </Routes>
+                        </div>
+                    </div>
+                
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
